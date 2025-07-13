7970417ad3ead8c0d1fbd63b35d762412ffa7d53
Pranav Kalaiselvan <pranavkalai04@gmail.com>
2025-07-12 20:26:13 -0500

    vehicle POST

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import axios from 'axios'
 
 const CustomForm = ({type}) => {
+  const [vehicleName, setVehicleName] = useState('');
+  const [registration, setRegistration] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8080/home/vehicles/add', { vehicleName, registration });    
+  }; 
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Vehicle Name</Form.Label>
-        <Form.Control placeholder={`${type} Vehicle Name`} />
-        {/* <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text> */}
+        <Form.Control placeholder={`${type} Vehicle Name`} onChange={ (e) => {setVehicleName(e.target.value)} }/>
+  
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label>Registration</Form.Label>
-        <Form.Control placeholder={`${type} Vehicle Name`} />
+        <Form.Control placeholder={`${type} Registration`} onChange={ (e) => {setRegistration(e.target.value)} }/>
       </Form.Group>
       <Button variant="primary" type="submit">
         {type}
