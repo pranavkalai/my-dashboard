01949f61be52e621c7fff103b937b9805d70deee
Pranav Kalaiselvan <pranavkalai04@gmail.com>
2025-07-15 23:57:56 -0500

    update

--- frontend/src/routes/Vehicles.jsx
+++ frontend/src/routes/Vehicles.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import Table from 'react-bootstrap/Table'
-import axios from 'axios'
-import './Vehicles.css'
-import Tab from '../components/tab/Tab'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Tab from '../components/tab/Tab';
+import Table from 'react-bootstrap/Table';
+import './Vehicles.css';
 
 
 const Vehicles = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); // data from backend when page loads
   useEffect(() => {
     axios.get('http://localhost:8080/home/vehicles')
       .then((res) => {
@@ -17,13 +18,14 @@ const Vehicles = () => {
       })
   }, []);
 
-  const formatedData = data.map((vehicle_info, index) => (
-      <tr key={vehicle_info.registration}>
+  const formatedData = data.map((vehicle_info, index) => ( // function formats data into table
+      <tr key={index}>
         <td>{index + 1}</td>
         <td>{vehicle_info.name}</td>
         <td>{vehicle_info.registration}</td>
         <td>Blank</td>
         <td>Blank</td>
+        <td><Link to={`/home/vehicles/view/id=${index}`}>View</Link></td> 
       </tr>
     )
   );
@@ -41,6 +43,7 @@ const Vehicles = () => {
                 <th>Registration</th>
                 <th>Last Service Date</th>
                 <th>Next Service Date</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
